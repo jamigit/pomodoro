7aa8a9fcd235998ebea522bf991386c85098da34
jamigit <jamiehbarter@gmail.com>
2017-02-27 20:14:15 +1300

    button functionality

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   sessionTime();
+  breakTime();
+  timer();
+
   //testClick();
 });
+
 
 /**
 function testClick(){
@@ -20,6 +24,55 @@ function testClick(){
 
 }
 **/
+
+
+var myInterval = null;
+
+
+function timer(){
+  $("#timerStart").click(
+    function(){
+        var sessionDuration = $("#sessionCounter").html();
+
+      var myInterval = setInterval(function(){
+          sessionDuration --;
+          $("#timerText").html(sessionDuration);
+          //alert(sessionDuration);
+
+          if(sessionDuration === 0){
+            alert("Your time is up!");
+            clearInterval(myInterval);
+          }
+
+            $("#timerReset").click(function(){
+              //alert("test");
+              clearInterval(myInterval);
+              $("#timerText").html(sessionDuration + " Stopped");
+            });
+
+
+  },1000);
+
+  }
+  )};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function sessionTime(){
@@ -33,6 +86,53 @@ function sessionTime(){
     countInt++;
     //alert(countInt);
       var displayCount = countInt.toString();
-     $("#sessionCounter").html(countInt);
+     $("#sessionCounter").html(displayCount);
   });
+
+  $("#sessionDown").click(
+    function(){
+     //alert("hi");
+     var countDown = $("#sessionCounter").html();
+
+     //alert(countDown);
+     var countDownInt = parseInt(countDown);
+    // alert(countDownInt);
+    countDownInt--;
+  //alert(countDownInt);
+     var displayCountDown = countDownInt.toString();
+    $("#sessionCounter").html(displayCountDown);
+  });
+
+
+};
+
+function breakTime(){
+  $("#breakUp").click(
+    function(){
+    //  alert("hi");
+      var count = $("#breakCounter").html();
+
+    //  alert(count);
+      var countInt = parseInt(count);
+    countInt++;
+    //alert(countInt);
+      var displayCount = countInt.toString();
+     $("#breakCounter").html(displayCount);
+  });
+
+  $("#breakDown").click(
+    function(){
+     //alert("hi");
+     var countDown = $("#breakCounter").html();
+
+     //alert(countDown);
+     var countDownInt = parseInt(countDown);
+    // alert(countDownInt);
+    countDownInt--;
+  //alert(countDownInt);
+     var displayCountDown = countDownInt.toString();
+    $("#breakCounter").html(displayCountDown);
+  });
+
+
 };
